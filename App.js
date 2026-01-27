869655db3134ffe91ef8691f203c1f2e3162291c
Ezeh Godwin <EzehGodwin3942@gmail.com>
2026-01-27 21:18:15 +0100

    updated cors

--- App.js
+++ App.js
@@ -44,8 +44,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://localhost:3000",
-    "http://localhost:3001",
+    "http://localhost:5174",
     "http://localhost:8080",
     "https://alth-world-front-end-fm6m-bzcajeud9-ezeh0415s-projects.vercel.app",
   );
