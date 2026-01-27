720ca26d4f22cb0bf65879bffda2b8bcb6c41497
Ezeh Godwin <EzehGodwin3942@gmail.com>
2026-01-28 00:27:34 +0100

    updated cors

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ dotenv.config();
 // Specific origin configuration
 const allowedOrigins = [
   "http://localhost:5174",
+  "http://localhost:5175",
   "http://localhost:8080",
   "https://alth-world-front-end-fm6m-bzcajeud9-ezeh0415s-projects.vercel.app",
 ];
