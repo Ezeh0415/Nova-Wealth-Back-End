461ea8a558432346f626347b1523e60829de3181
Ezeh Godwin <EzehGodwin3942@gmail.com>
2026-06-19 07:57:13 +0100

    transiction to ts

---
+++ src/config/Config.ts
@@ -0,0 +1,21 @@
+export class AppConfig {
+    private static instance: AppConfig;
+    private _port: number;
+
+    private constructor() {
+        this._port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
+    }
+
+    public static getInstance(): AppConfig {
+        if (!AppConfig.instance) {
+            AppConfig.instance = new AppConfig();
+        }
+        return AppConfig.instance;
+    }
+
+    public get port(): number {
+        return this._port;
+    }
+}
+
+export default AppConfig.getInstance();
